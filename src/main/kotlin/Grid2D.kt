5ee8b1d164ae3896c3cddf26f63aae306e264d9e
Matteo Martinelli <teo.martinelli97@gmail.com>
2021-09-13 16:42:38 +0200

    Documentation for Grid2D.kt

--- src/main/kotlin/Grid2D.kt
+++ src/main/kotlin/Grid2D.kt
@@ -1,16 +1,51 @@
 import kotlin.random.Random
 
+/**
+ * 2D grid container as a graph
+ *
+ * This class builds a graph from a 2D grid of squares, where each square is a node, and the length of each edge
+ * is 1.0.
+ * 2 adjacent squares are considered also adjacent nodes.
+ * Since [Graph] works with node IDs, also [Grid2D] does, but provides to the user a simplified interface,
+ * with rows and cols indices, as one would expect on a grid.
+ *
+ * IDs are assigned from left to right, top to bottom increasingly.
+ * Same applies for rows and cols. (0, 0) is the top-left square, ([nSquares]-1, [nSquares]-1) is the bottom-right one.
+ *
+ * Some nodes may be considered as blocks, meaning there cannot be any edge with them. This is meant to apply
+ * some difficulties in the finding of the shortest path.
+ *
+ * It is also possible to create a grid where blocks are randomly positioned, with the [Boolean] flag [randomBlocks].
+ * If true, each node has 10% of probability of becoming a block. This is compatible also with giving deterministic
+ * blocks.
+ *
+ * Class properties are:
+ * - [nSquares]: Number of squares per side in the grid. (Sets the dimension of the grid)
+ * - [blocks]: List of the position
+ * - [randomBlocks]: Boolean for creating a random grid.
+ *
+ * - [graph]: The [Graph] representing the grid
+ * - [freeV]: The nodes that are not blocks
+ *
+ * @see Graph
+ */
+
 class Grid2D(
     val nSquares: Int,
     val blocks: MutableList<Pair<Int, Int>> = mutableListOf(),
     val randomBlocks : Boolean = false
 ) {
+
     private val V = (0 until nSquares * nSquares).toMutableList()
     val graph = Graph(V, directed = false)
     val freeV : MutableList<Pair<Int, Int>> = mutableListOf()
     private val rand = Random.Default
-    init {
 
+    /**
+     * Initialization of graph. Edges between nodes are built, considering if there are blocks or not.
+     * If [randomBlocks] is true a random generators decides whether each square may become a block
+     */
+    init {
         for (i in 0 until nSquares * nSquares) {
             val row = i / nSquares
             val col = i % nSquares
@@ -33,6 +68,10 @@ class Grid2D(
         }
     }
 
+    /**
+     * Adds a block in position ([row], [col]). Each edge with this node as an end is removed, the node is
+     * added to [blocks] and it is removed from [freeV]
+     */
     fun addBlock(row: Int, col: Int) {
         blocks.add(Pair(row, col))
         val id = nSquares * row + col
@@ -40,7 +79,13 @@ class Grid2D(
         freeV.remove(Pair(row, col))
     }
 
+    /**
+     * Returns the ID value of the square, given the coordinates as (row, col)
+     */
     fun getId(row: Int, col : Int) : Int {return nSquares * row + col}
+    /**
+     * Returns the coordinates value as a [Pair] = (row, col), given the ID value of the square.
+     */
     fun getCoordinates(id : Int) : Pair<Int, Int> {
         val row = id / nSquares
         val col = id % nSquares
