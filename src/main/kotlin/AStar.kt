5cb538817477c6a5ed73993bd98a0701d2c8def3
Matteo Martinelli <teo.martinelli97@gmail.com>
2021-09-17 16:14:10 +0200

    Fixed A*

--- src/main/kotlin/AStar.kt
+++ src/main/kotlin/AStar.kt
@@ -10,6 +10,7 @@ object AStar {
             throw RuntimeException("Target ($target) is not in graph vertices, cannot compute path!")
         }
         if (target == source) return ArrayDeque(target)
+
         val prev = mutableMapOf<Int, Int>()
         val dist = mutableMapOf<Int, Float>()
         dist[source] = 0f
@@ -18,16 +19,11 @@ object AStar {
             g.V.size,
             compareBy { it.second }
         )
-        val closed = PriorityQueue<Pair<Int, Float>>(
-            g.V.size,
-            compareBy { it.second }
-        )
-        open.add(Pair(source, dist[source]!! + heuristic(source, source)))
+        open.add(Pair(source, heuristic(source, target)))
 
         while (!open.isEmpty()) {
             //Choose min distance vertex and remove from open list
             val u = open.poll()
-            closed.add(u)
             if (u.first == target) {break}
             //For each neighbour of u
             // evaluate distance from source with intermediate step 'u'
@@ -38,8 +34,8 @@ object AStar {
                 if (it.first !in dist.keys || newDist < dist[it.first]!!) {
                     prev[it.first] = u.first
                     dist[it.first] = newDist
-                    open.remove(it)     //If in open queue -> remove
-                    open.add(Pair(it.first, newDist + heuristic(source, it.first)))
+//                    open.remove(it)     //If in open queue -> remove and add again with new distance
+                    open.add(Pair(it.first, newDist + heuristic(it.first, target)))
                 }
             }
         }
@@ -55,18 +51,66 @@ object AStar {
     /**
      * Rebuilding path from source to target, given the Map of previous vertex along the desired path
      */
+
     private fun buildPath (source : Int, target : Int, prev : MutableMap<Int, Int>) : ArrayDeque<Int> {
         val path = ArrayDeque<Int>()
         var u = target
         path.add(u)
-        while (prev[u] != null) {
+        while (prev[u] != null && prev[u] != source) {
             path.addFirst(prev[u]!!)
-            if (prev[u] == source) {
-                println(prev[u])
-                break
-            }
             u = prev[u]!!
         }
+        path.addFirst(source)
         return path
+    }
+
+
+    fun visualTargetShortestPath (g: Graph<Int>, source: Int, target : Int, heuristic: (Int, Int) -> Float) : Pair<ArrayDeque<Int>, MutableList<Int>> {
+        if (source !in g.V) {
+            throw RuntimeException("Source ($source) is not in graph vertices, cannot compute path!")
+        }
+        if (target !in g.V) {
+            throw RuntimeException("Target ($target) is not in graph vertices, cannot compute path!")
+        }
+        val visitedNodes = mutableListOf(source)
+        if (target == source) return Pair(ArrayDeque(target), visitedNodes)
+
+
+        val prev = mutableMapOf<Int, Int>()
+        val dist = mutableMapOf<Int, Float>() //distance from source
+        dist[source] = 0f
+
+        val open = PriorityQueue<Pair<Int, Float>>(
+            g.V.size,
+            compareBy { it.second }
+        )
+        open.add(Pair(source, heuristic(source, target)))
+
+        while (!open.isEmpty()) {
+            //Choose min distance vertex and remove from open list
+            val u = open.poll()
+            visitedNodes.add(u.first)
+            if (u.first == target) {break}
+            //For each neighbour of u
+            // evaluate distance from source with intermediate step 'u'
+            g.neighbours[u.first]?.forEach {
+                val newDist = dist[u.first]!! + it.second
+                //If lesser than before (or first path for that vertex), and update distance
+                // and remember previous vertex (u)
+                if (it.first !in dist.keys || newDist < dist[it.first]!!) {
+                    prev[it.first] = u.first
+                    dist[it.first] = newDist
+//                    open.remove(it)     //If in open queue -> remove and add again with new distance
+                    open.add(Pair(it.first, newDist + heuristic(it.first, target)))
+                }
+            }
+        }
+        return Pair(buildPath(source, target, prev), visitedNodes)
+    }
+
+    fun visualTargetShortestPath (g: Grid2D, source: Int, target : Int, heuristic: (Int, Int) -> Float) : Pair<ArrayDeque<Int>, MutableList<Int>> {
+        if (g.getCoordinates(source) in g.blocks) throw java.lang.RuntimeException("Given source is a block." +
+                " Cannot start finding shortest path")
+        return visualTargetShortestPath(g.graph, source, target, heuristic)
     }
 }
